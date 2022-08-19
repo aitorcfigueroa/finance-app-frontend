d8fb840fb63c888a83e8dfa7b3b771ae6f67ece3
Aitor C. Figueroa <aitorcf@gmail.com>
2022-08-19 15:14:35 +0200

    Register form routes with MUI done

---
+++ src/pages/RegisterPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import RegisterForm from '../components/forms/RegisterForm.js';
+
+export const RegisterPage = () => {
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  )
+}
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,14 +1,14 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 //import { HomePage } from '../pages/HomePage';
 import { LoginPage } from '../pages/LoginPage';
-//import { RegisterPage } from '../pages/RegisterPage';
+import { RegisterPage } from '../pages/RegisterPage';
 
 export const AppRoutes = () => {
   return (
     <Routes>
       {/* <Route path='/' element={<HomePage />}></Route> */}
       <Route path='/login' element={<LoginPage />}></Route>
-      {/* <Route path='/register' element={<RegisterPage />}></Route> */}
+      <Route path='/register' element={<RegisterPage />}></Route>
       {/* Redirect when Page Not Found */}
       <Route path='*' element={<Navigate to='/' replace/>}></Route>
     </Routes>
