a6a91861738848e1403b2a0b98a4898e3db9265f
Aitor C. Figueroa <aitorcf@gmail.com>
2022-08-22 13:32:59 +0200

    Menu users panel visual done

--- src/components/home/profile.js
+++ src/components/home/profile.js
@@ -3,6 +3,11 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import HomeIcon from '@mui/icons-material/HomeIcon';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ReorderIcon from '@mui/icons-material/Reorder';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 // TODO: Petición para recibir los datos personales Nombre, ID, DOB
 
@@ -45,8 +50,31 @@ export const ProfileMenu = () => {
           </Grid>
         </Grid>
       </Paper>
-      <Grid container>
-        {/* TODO: Añadir Dashboard, Details, Movimientos, Configuración */}
+      <Grid container sx={{ maxWidth: 300, p: 1, gap: 3}} direction="column" alignItems="flex-start">
+        <Grid item>
+          <Button sx={{gap: 1}}>
+            <HomeIcon />
+            Home
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button sx={{gap: 1}}>
+            <DashboardIcon />
+            Details
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button sx={{gap: 1}}>
+            <ReorderIcon />
+            Movements
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button sx={{gap: 1}}>
+            <SettingsIcon />
+            Configuration
+          </Button>
+        </Grid>
       </Grid>
     </Box>
   )
